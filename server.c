240d4ed8221ff3252e350c0d82cde6cabfe61eea
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-08-01 12:29:18 +0200

    server cleint files were created

---
+++ server.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define PORT 9090
+#define LISTEN_BACKLOG 50
+#define BUFFER_SIZE 1024
+
+void error(const char* msg){
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
+
+int main(){
+
+	//0 Für Protocl, das ist einfach deafult
+	int serverSocket = socket(AF_INET, SOCK_STREAM,0);
+	if(serverSocket == -1){
+		error("Error creating the socket");
+	}
+
+	struct sockaddr_in serverSocketAdress;
+	memset(&serverSocketAdress, 0, sizeof(serverSocketAdress));
+	serverSocketAdress.sin_family = AF_INET;
+	serverSocketAdress.sin_port = htons(PORT);
+	serverSocketAdress.sin_addr.s_addr = INADDR_ANY;
+	
+
+	if(bind(serverSocket,(struct sockaddr*) &serverSocketAdress ,sizeof(serverSocketAdress)) == -1){
+		error("Binding the socket to an adress failed");
+	}
+
+	if(listen(serverSocket,LISTEN_BACKLOG) == -1){
+		error("Error listening for a connection");
+	}
+
+	//ACCEPT ...
+
+	struct sockaddr_in clientAddr;
+	socklen_t clientLen = sizeof(clientAddr);
+
+	int clientSocket = accept(serverSocket, (struct sockaddr*)& clientAddr, &clientLen);
+
+
+	if(clientSocket == -1){
+		error("Error accpeting the connection");
+	}
+
+	const char* message = "Heyy";
+
+	if(send(clientSocket, message, strlen(message), 0) == -1){
+		error("Error sending the data");
+	}
+
+	char buffer[BUFFER_SIZE] = {0};
+
+	if(recv(clientSocket, buffer, sizeof(buffer), 0) < 0){
+		error("Error reciving the data from the client");
+	}
+	
+
+	//if(inet_pton(AF_INET, "127.0.0.1", &socketAdress.sin_addr) <= 0){
+    //    error("Invalid address/ Address not supported");
+    //}
+	
+	//if(connect(serverSocket,(struct sockaddr*) &socketAdress, sizeof(socketAdress)) == -1){
+	//	error("Error, conneting the socket with the server failed");
+	//}
+
+	close(serverSocket);
+	
+
+	return 0;
+}
